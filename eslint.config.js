f356e914ff96515e09466a069086cf467986dd89
Curiosty19 <64819354+Curiosty19@users.noreply.github.com>
2024-12-15 22:11:17 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
 // eslint.config.js
-module.exports = {
-  languageOptions: {
-    globals: {
-      browser: "readonly", // or "writable" if you want to modify it
-      es2021: "readonly",
+import eslintRecommended from 'eslint:recommended';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        browser: 'readonly', // or 'writable' if you want to modify it
+        es2021: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
     },
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "module",
+    extends: [eslintRecommended], // Including the recommended config directly
+    rules: {
+      // Add your custom rules here
     },
   },
-  extends: ["eslint:recommended"],
-  rules: {
-    // Add your custom rules here
-  },
-};
+];
